de218aa21f5534dceac72b60be5ada3c3d712415
Ayooon16 <102410130+Ayooon16@users.noreply.github.com>
2026-06-14 03:15:30 +0200

    Fix weekday counting

--- backend/tasks.php
+++ backend/tasks.php
@@ -9,13 +9,13 @@ $id = $_SESSION['user_id'];
 $conn = getMysqliConnection();
 if ($conn->connect_error) die("Connection failed");
 $weekdays = [
-  'Monday',
-  'Tuesday',
-  'Wednesday',
-  'Thursday',
-  'Friday',
-  'Saturday',
-  'Sunday'
+  1 => 'Monday',
+  2 => 'Tuesday',
+  3 => 'Wednesday',
+  4 => 'Thursday',
+  5 => 'Friday',
+  6 => 'Saturday',
+  7 => 'Sunday'
 ];
 $showOld = true;
 if (isset($_POST["noOld"])) {
